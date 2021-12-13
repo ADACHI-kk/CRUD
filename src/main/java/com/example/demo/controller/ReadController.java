a6eca1b1b6d75a1264190d6b210b1cb23f56f8d7
ADACHI <adc.nt1011@gmail.com>
2021-12-13 16:20:40 +0900

    test

---
+++ src/main/java/com/example/demo/controller/ReadController.java
@@ -0,0 +1,57 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.dto.UserSearchRequest;
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+
+@Controller
+public class ReadController {
+
+	@Autowired
+	UserService userService;
+
+	@GetMapping(value = "user/search")
+	public String displaySearch(Model model) {
+		return "user/search";
+	}
+
+	/**
+	 * ユーザー情報検索
+	 * 
+	 * @param userSearchRequest リクエストデータ
+	 * @param model             Model
+	 * @return ユーザー情報一覧画面
+	 */
+	@RequestMapping(value = "/user/id_search", method = RequestMethod.POST)
+	public String search(@ModelAttribute UserSearchRequest userSearchRequest, Model model) {
+		User user = userService.search(userSearchRequest);
+		model.addAttribute("userinfo", user);
+		return "user/search";
+	}
+	
+	@GetMapping("/user/register")
+    public String registerUser(Model model, @ModelAttribute User u) {
+        model.addAttribute("user", u);
+        return "user/register";
+    }
+
+	@PostMapping("/user/register")
+	public String create(@Validated @ModelAttribute User u, BindingResult result) {
+		if (result.hasErrors()) {
+			return "user/register";
+		}
+		userService.insertOne(u);
+		return "redirect:/user/search";
+	}
+}
---
+++ src/main/java/com/example/demo/repository/UserMapper.java
@@ -0,0 +1,20 @@
+package com.example.demo.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.dto.UserSearchRequest;
+import com.example.demo.entity.User;
+
+@Mapper
+public interface UserMapper {
+    /**
+     * ユーザー情報検索
+     * @param user 検索用リクエストデータ
+     * @return ユーザー情報
+     */
+    User search(UserSearchRequest user);
+    
+    
+    //insert
+    public void insertOne(User u);
+}
